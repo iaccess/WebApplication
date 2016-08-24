7e52de3bbeac249b4076e84657d728c8061f1f32
gabbydgab <gamba@gabbydgab.com>
2016-08-24 11:57:31 +0800

    added dashboard template related settings

--- src/Application/ConfigProvider.php
+++ src/Application/ConfigProvider.php
@@ -41,12 +41,24 @@ final class ConfigProvider
 
         return [
             'map'   => [
+                //Site related settings
                 'layout/layout'             => $path . "/layout/default.phtml",
                 'partial/header'            => $path . '/layout/partial/header.phtml',
                 'partial/footer'            => $path . '/layout/partial/footer.phtml',
                 'widget/banner/footer'      => $path . '/layout/widget/banner/footer.phtml',
                 'template/navigation'       => $path . '/layout/template/navigation.phtml',
                 'template/breadcrumbs'      => $path . '/layout/template/breadcrumbs.phtml',
+
+                //Dashboard related settings
+                'layout/dashboard'          => $path . '/layout/dashboard.phtml',
+                'template/navigation/sidebar'       => $path . '/layout/template/navigation/sidebar.phtml',
+                'template/navigation/header'        => $path . '/layout/template/navigation/header.phtml',
+                'template/navigation/breadcrumbs'   => $path . '/layout/template/navigation/breadcrumbs.phtml',
+                'partial/navigation/header'         => $path . '/layout/partial/navigation/header.phtml',
+                'partial/navigation/sidebar'        => $path . '/layout/partial/navigation/sidebar.phtml',
+                'partial/navigation/breadcrumbs'    => $path . '/layout/partial/navigation/breadcrumbs.phtml',
+                'partial/footer'                    => $path . '/layout/partial/footer.phtml',
+                'boxcar/small'                      => $path . '/layout/widget/boxcar/small.phtml'
             ],
             'paths' => [
                 'error'     => [$path . '/error'],
